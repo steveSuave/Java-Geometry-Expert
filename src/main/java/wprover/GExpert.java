f8c74bd6722222ebe6e267738ba5db14678d99bf
Your Name <s.levantes@gmail.com>
2025-10-20 16:24:46 +0300

    Change translate tool shortcut

--- src/main/java/wprover/GExpert.java
+++ src/main/java/wprover/GExpert.java
@@ -4644,7 +4644,7 @@ public class GExpert extends JFrame implements ActionListener, KeyListener, Drop
         bindKey(KeyStroke.getKeyStroke(KeyEvent.VK_A, 0), "SelectAngleTool", "angle", "Angle");
         bindKey(KeyStroke.getKeyStroke(KeyEvent.VK_M, 0), "SelectMoveTool", "move", "Move");
         bindKey(KeyStroke.getKeyStroke(KeyEvent.VK_I, 0), "SelectIntersectTool", "intersect", "Intersect");
-        bindKey(KeyStroke.getKeyStroke(KeyEvent.VK_P, 0), "SelectTranslateTool", "translate", "Translate");
+        bindKey(KeyStroke.getKeyStroke(KeyEvent.VK_0, 0), "SelectTranslateTool", "translate", "Translate");
         bindKey(KeyStroke.getKeyStroke(KeyEvent.VK_F, 0), "FootTool", "foot", "Foot");
         bindKey(KeyStroke.getKeyStroke(KeyEvent.VK_N, 0), "Fill Polygon", "fill polygon", "Fill Polygon");
         bindKey(KeyStroke.getKeyStroke(KeyEvent.VK_T, 0), "Triangle", "triangle", "Triangle");
